8030362b258242dbbc7c962f48359d6403cc7060
Abdou Kaira <gambii@Abdous-MacBook-Pro.local>
2021-08-05 02:16:28 -0400

    push starting files onto remote repo

---
+++ src/Pages/RocMap/component/RocMap.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom';
+
+
+
+
+const RocMap = (props) => {
+    return (
+        <h1>React Map {props.name}</h1>
+    );
+}
+
+
+export default RocMap;
---
+++ src/Pages/RocMap/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import RocMap from './component/RocMap'
+
+
+const RenderRocMapPage = () => {
+    <>
+        <RocMap></RocMap>
+    </>
+}
+
+export default RenderRocMapPage;
